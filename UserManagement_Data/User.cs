c7bdd87d48b5b9b61a34c1f78f7d1332f64b7b86
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-03-08 11:13:26 +0000

    2-standard-add a new property to the User class

--- UserManagement_Data/User.cs
+++ UserManagement_Data/User.cs
@@ -5,6 +5,7 @@ public class User
     public long Id { get; set; }
     public string? Forename { get; set; }
     public string? Surname { get; set; }
+    public string? DateOfBirth { get; set; }
     public string? Email { get; set; }
     public bool IsActive { get; set; }
 }
