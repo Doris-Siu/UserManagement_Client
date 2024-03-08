41b39a2c6aeca31c45db4dc994a9c499d336d33d
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-03-08 02:11:07 +0000

    migrate codes, override the default by Blazor Bootstrap

---
+++ UserManagement_Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿// implement DB later
+
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+
+
+//namespace Tangy_DataAccess.Data
+//{
+//	public class ApplicationDbContext
+//	{
+//		public ApplicationDbContext()
+//        {
+
+//        }
+
+//        public DbSet<User> Users { get; set; }
+//    }
+
+
+
+//}
+
---
+++ UserManagement_Data/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagement_Data;
+
+public class User
+{
+    public long Id { get; set; }
+    public string? Forename { get; set; }
+    public string? Surname { get; set; }
+    public string? Email { get; set; }
+    public bool IsActive { get; set; }
+}
