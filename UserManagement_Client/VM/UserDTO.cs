97b724bbd8e244ce907e63d97b12796146467c89
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-03-08 13:05:56 +0000

    3-standard-create Add, Edit UI flows plus validation

---
+++ UserManagement_Client/VM/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement_Client.VM
+{
+	public class UserDTO
+	{
+        public long Id { get; set; }
+
+        [Required]
+        public string? Forename { get; set; }
+
+        [Required]
+        public string? Surname { get; set; }
+
+        [Required]
+        public string? DateOfBirth { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
+
