14b198ba19351cb99a010932e5f4b0d81ba25188
Doris <107772913+Doris-Siu@users.noreply.github.com>
2024-03-12 23:24:13 +0000

    add data validation

--- UserManagement_Client/DtoModel/UserDTO.cs
+++ UserManagement_Client/DtoModel/UserDTO.cs
@@ -13,11 +13,13 @@ namespace UserManagement_Client.DtoModel
         [Required]
         public string? Surname { get; set; }
 
-        [Required]
-        public string? DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
 
-        [Required]
-        public string? Email { get; set; }
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "Date of Birth must be in format YYYYMMDD which is  8 digits")]
+        public string DateOfBirth { get; set; }
 
         public bool IsActive { get; set; }
     }
